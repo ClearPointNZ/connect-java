385e0a6bb04a8b0b5c8e322521443a70363e8c96
Richard Vowles <richard@bluetrainsoftware.com>
2021-01-12 17:44:07 +1300

    moving to support 5.0

---
+++ maven-plugins/openapi-jersey2-plugin/src/test/java/cd/connect/openapi/SampleTest.java
@@ -0,0 +1,20 @@
+package cd.connect.openapi;
+
+import org.junit.Test;
+import org.openapitools.codegen.OpenAPIGenerator;
+
+import java.util.Arrays;
+
+public class SampleTest {
+  @Test
+  public void runGenerator() {
+    String location = getClass().getResource("/sample2.yaml").getFile();
+    OpenAPIGenerator.main(Arrays.asList("generate",
+      "--input-spec", location,
+      "--generator-name", "jersey2-api",
+      "--api-package", "api",
+      "--model-package", "model",
+      "--output", "target/" + getClass().getSimpleName())
+      .toArray(new String[0])); }
+
+}
